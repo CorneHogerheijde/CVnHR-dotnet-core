6e3970ee423288a5621892c3115f9bc86631258b
corne.hogerheijde@qnh.nl <corne.hogerheijde@qnh.nl>
2018-07-25 14:52:32 +0200

    feat(settings): Added settings page

--- src/CVnHR.manage/ClientApp/src/components/NavMenu.js
+++ src/CVnHR.manage/ClientApp/src/components/NavMenu.js
@@ -5,31 +5,36 @@ import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
 export default props => (
-  <Navbar inverse fixedTop fluid collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to={'/'}>CVnHR.manage</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <LinkContainer to={'/'} exact>
-          <NavItem>
-            <Glyphicon glyph='home' /> Home
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/counter'}>
-          <NavItem>
-            <Glyphicon glyph='education' /> Counter
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/fetchdata'}>
-          <NavItem>
-            <Glyphicon glyph='th-list' /> Fetch data
-          </NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+    <Navbar inverse fixedTop fluid collapseOnSelect>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <Link to={'/'}>CVnHR.manage</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav>
+                <LinkContainer to={'/'} exact>
+                    <NavItem>
+                        <Glyphicon glyph='home' /> Home
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/counter'}>
+                    <NavItem>
+                        <Glyphicon glyph='education' /> Counter
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/fetchdata'}>
+                    <NavItem>
+                        <Glyphicon glyph='th-list' /> Fetch data
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/settings'}>
+                    <NavItem>
+                        <Glyphicon glyph='cog' /> Settings
+                    </NavItem>
+                </LinkContainer>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 );
---
+++ src/CVnHR.manage/ClientApp/src/components/Settings.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+
+const Settings = props => (
+    <div>
+        <h1>Settings</h1>
+        <h2>Certificates</h2>
+        <p>
+            Add your certificates here.
+        </p>
+    </div>
+);
+
+export default connect()(Settings);

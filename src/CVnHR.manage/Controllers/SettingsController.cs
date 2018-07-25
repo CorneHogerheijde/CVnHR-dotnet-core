537b72c24d413ce0b9830607eef709bd6458c2a9
corne.hogerheijde@qnh.nl <corne.hogerheijde@qnh.nl>
2018-07-25 16:02:25 +0200

    feat(settings): List installed certificates on settings page

---
+++ src/CVnHR.manage/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVnHR.manage.Controllers
+{
+    [Route("api/[controller]")]
+    public class SettingsController : Controller
+    {
+        private const string CertificatesPath = "Certificates";
+
+        [HttpGet("[action]")]
+        public IEnumerable<string> GetCertificates()
+        {
+            return Directory.GetFiles(CertificatesPath, "*.pfx")
+                    .Select(p=> p.Replace(CertificatesPath+"\\", string.Empty).Replace(".pfx", string.Empty));
+        }
+    }
+}

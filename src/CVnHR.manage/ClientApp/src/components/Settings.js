5b99155b14b369ab3eb4834ffcc6966046615987
corne.hogerheijde@qnh.nl <corne.hogerheijde@qnh.nl>
2018-07-25 16:02:25 +0200

    feat(settings): List installed certificates on settings page

--- src/CVnHR.manage/ClientApp/src/components/Settings.js
+++ src/CVnHR.manage/ClientApp/src/components/Settings.js
@@ -1,14 +1,39 @@
-﻿import React from 'react';
+﻿import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { actionCreators } from '../store/Settings';
 
-const Settings = props => (
-    <div>
-        <h1>Settings</h1>
-        <h2>Certificates</h2>
-        <p>
-            Add your certificates here.
-        </p>
-    </div>
-);
+class Settings extends Component {
+    componentWillMount() {
+        this.props.requestCertificates();
+    }
 
-export default connect()(Settings);
+    render() {
+        return (
+            <div>
+                <h1>Settings</h1>
+                <h3>Certificates</h3>
+                <p>
+                    Current installed certificate(s):
+                </p>
+
+                <ul>
+                    {this.props.settings.certificates.length > 0
+                        && this.props.settings.certificates.map(certificate =>
+                           <li>{certificate}</li>
+                        )}
+                </ul>
+                {this.props.settings.certificates.length == 0 &&
+                    <p>No Certificate found!
+                        Add pfx file in Certificates location if no certificate listed here.
+                    </p>}
+                <p><strong>TODO: allow upload and edit of certificate</strong></p>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    state => state.settings,
+    dispatch => bindActionCreators(actionCreators, dispatch)
+)(Settings);

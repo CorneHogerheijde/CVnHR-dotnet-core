27bfc4e90de667df0661651786e208a3797ef382
corne.hogerheijde@qnh.nl <corne.hogerheijde@qnh.nl>
2018-11-23 16:14:14 +0100

    Refactor

--- src/CVnHR.Business/Kvk/HRDataserviceMessageParser.cs
+++ src/CVnHR.Business/Kvk/HRDataserviceMessageParser.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.Serialization;
-using System.ServiceModel.Channels;
-using System.Text;
-using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 
@@ -23,20 +17,14 @@ namespace CVnHR.Business.Kvk
             var type = typeof(MaatschappelijkeActiviteitResponseType);
             var serializer = new XmlSerializer(type, ns);
                 
-            var nodeNameM = "MaatschappelijkeActiviteitResponseType";
-            var xDoc = XDocument.Parse(message.Replace(typeof(ophalenInschrijvingResponse).Name, nodeNameM));
-            var nodeName = XName.Get(nodeNameM, "http://schemas.kvk.nl/schemas/hrip/dataservice/2015/02");
+            var xDoc = XDocument.Parse(message.Replace(typeof(ophalenInschrijvingResponse).Name, type.Name));
+            var nodeName = XName.Get(type.Name, ns);
             var ophalenInschrijvingResponseNode = xDoc.Descendants(nodeName);
             var ophalenInschrijvingResponseXml = ophalenInschrijvingResponseNode.FirstOrDefault();
             using (var reader = ophalenInschrijvingResponseXml.CreateReader())
             {
-
-                if (serializer.CanDeserialize(reader))
-                {
-                    var result = serializer.Deserialize(reader);
-                    return (MaatschappelijkeActiviteitResponseType)result;
-                }
-                throw new Exception("Cannot deserialize the message.");
+                var result = serializer.Deserialize(reader);
+                return (MaatschappelijkeActiviteitResponseType)result;
             }
         }
     }
